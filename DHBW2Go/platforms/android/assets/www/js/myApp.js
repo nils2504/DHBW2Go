01b30a53e4e53b4daa10bb6d855a31a42a88c3e1
nils2504 <nils.rhode04@gmail.com>
2014-03-15 10:00:27 +0100

    myApp Alerts weg

--- DHBW2Go/platforms/android/assets/www/js/myApp.js
+++ DHBW2Go/platforms/android/assets/www/js/myApp.js
@@ -9,21 +9,11 @@ localdb.transaction(makeQueryUser,errorCB);
 document.addEventListener("offline", onOffline, false);
 document.addEventListener("online", onOnline, false);
 document.addEventListener("deviceready", onDeviceReady, false);
-checkCon();
+
 
 function onDeviceReady() {
-	alert("deviceready");
-
-
 }
 
-	function onOffline() {
-    	alert("offline");
-	}
-
-	function onOnline() {
-		alert("online");
-	}
 
 function rememberMe(){
 if(userObjekt[6] == 1){
@@ -406,18 +396,4 @@ function out(){
 	Parse.User.logOut();
 	document.location.href = "index.html";
 	
-}
-function checkCon(){
-	alert("CHECK");
-    var networkState = navigator.network.connection.type;
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.NONE]     = 'No network connection';
-	alert(states[networkState]);
-
 }
